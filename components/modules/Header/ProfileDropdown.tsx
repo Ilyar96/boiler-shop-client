52d2a961e6719f9527192a77c09ad05a09423e5b
Ilyar <kasyymov.ilyar@gmail.com>
2023-06-23 10:05:56 +0300

    add logout handler for ProfileDropdown

--- components/modules/Header/ProfileDropdown.tsx
+++ components/modules/Header/ProfileDropdown.tsx
@@ -1,5 +1,6 @@
 import React, { forwardRef } from 'react'
 import { useStore } from 'effector-react'
+import { useRouter } from 'next/router'
 import { AnimatePresence, motion } from 'framer-motion'
 import cn from 'classnames'
 import { IWrappedComponentProps } from '@/types/common'
@@ -8,13 +9,20 @@ import { $user } from '@/context/user'
 import ProfileSvg from '@/components/elements/ProfileSvg/ProfileSvg'
 import LogoutSvg from '@/components/elements/LogoutSvg/LogoutSvg'
 import { withClickOutside } from '@/utils/withClickOutside'
+import { logoutFx } from '@/app/api/auth'
 import styles from '@/styles/profileDropdown/index.module.scss'
 
 const ProfileDropdown = forwardRef<HTMLDivElement, IWrappedComponentProps>(
   ({ open, setOpen }, ref) => {
     const mode = useStore($mode)
     const user = useStore($user)
+    const router = useRouter()
     const toggleProfileDropdown = () => setOpen(!open)
+
+    const handleLogout = async () => {
+      await logoutFx('/users/logout')
+      router.push('/')
+    }
 
     return (
       <div
@@ -46,7 +54,10 @@ const ProfileDropdown = forwardRef<HTMLDivElement, IWrappedComponentProps>(
                 </span>
               </li>
               <li className={styles.profile__dropdown__item}>
-                <button className={styles.profile__dropdown__item__btn}>
+                <button
+                  className={styles.profile__dropdown__item__btn}
+                  onClick={handleLogout}
+                >
                   <span className={styles.profile__dropdown__item__text}>
                     Выйти
                   </span>
